5e65f07551a916ffa6278a902444db99d01bb0e5
Deraybcbc <a22kevburcac@inspedralbes.cat>
2024-02-21 13:26:59 +0100

    android

---
+++ peliculas/src/views/Tab1Page.vue
@@ -0,0 +1,41 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>BUSCADOR DE PELICULAS</ion-title>
+      </ion-toolbar>
+
+      <ion-toolbar class="ion-padding">
+        <ion-searchbar placeholder="Nombre película" @ionChange="BuscarBoton"></ion-searchbar>
+      </ion-toolbar>
+
+    </ion-header>
+
+    <ion-content :fullscreen="true" class="ion-padding">
+
+
+      <ion-card>
+        <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+        <ion-card-header>
+          <ion-card-title>Card Title</ion-card-title>
+          <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+        </ion-card-header>
+
+        <ion-card-content>
+          Here's a small text description for the card content. Nothing more, nothing less.
+        </ion-card-content>
+      </ion-card>
+
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+
+const BuscarBoton = () =>{
+}
+
+</script>
